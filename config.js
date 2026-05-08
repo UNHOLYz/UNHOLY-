82143434efb03bef3dcf1d99ee940da383915ae4
UNHOLYz <signore376@gmail.com>
2026-05-08 10:38:33 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// ==========================================
+// CONFIG FILE
+// ==========================================
+
+module.exports = {
+  // Owner Number
+  // Example: "2348012345678"
+  OWNER_NUMBER:
+    process.env.OWNER_NUMBER || "2349121104372",
+
+  // Admin Numbers
+  // Add admins here
+  ADMINS: [
+    "2349121104372@s.whatsapp.net",
+    // "2349000000000@s.whatsapp.net"
+  ],
+};
